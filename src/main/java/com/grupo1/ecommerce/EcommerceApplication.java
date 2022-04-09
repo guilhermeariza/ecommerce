efc669e575d5ccf867bac419708fa9663fc2cb51
Guilherme Ariza <gui.ariza@gmail.com>
2022-04-08 22:44:52 -0300

    commit gui

--- src/main/java/com/grupo1/ecommerce/EcommerceApplication.java
+++ src/main/java/com/grupo1/ecommerce/EcommerceApplication.java
@@ -3,6 +3,9 @@ package com.grupo1.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//comentario gui git
+
 @SpringBootApplication
 public class EcommerceApplication {
 
