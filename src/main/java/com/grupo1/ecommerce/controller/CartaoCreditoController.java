901371752cb9843ccdd46a9c499abe87103a1b8c
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-04-13 15:21:41 -0300

    Metodos adicionados a CartaoCreditoController

--- src/main/java/com/grupo1/ecommerce/controller/CartaoCreditoController.java
+++ src/main/java/com/grupo1/ecommerce/controller/CartaoCreditoController.java
@@ -3,9 +3,15 @@ package com.grupo1.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +30,31 @@ public class CartaoCreditoController {
 	public ResponseEntity<List<CartaoCredito>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CartaoCredito> getById(@PathVariable Long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("apelido/{apelido}")
+	public ResponseEntity<List<CartaoCredito>> getByApelido(@PathVariable String apelido){
+		return ResponseEntity.ok(repository.findAllApelidoContainingIgnoreCase(apelido));
+	}
+	
+	@PostMapping
+	public ResponseEntity<CartaoCredito> post(@RequestBody CartaoCredito cartao){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(cartao));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CartaoCredito> put(@RequestBody CartaoCredito cartao){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(cartao));
+	}
 
+	@DeleteMapping
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
--- src/main/java/com/grupo1/ecommerce/repository/CartaoCreditoRepository.java
+++ src/main/java/com/grupo1/ecommerce/repository/CartaoCreditoRepository.java
@@ -1,5 +1,7 @@
 package com.grupo1.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.grupo1.ecommerce.model.CartaoCredito;
 
 @Repository
 public interface CartaoCreditoRepository extends JpaRepository<CartaoCredito, Long>{
+
+	public List<CartaoCredito> findAllApelidoContainingIgnoreCase(String apelido);
 
 }
