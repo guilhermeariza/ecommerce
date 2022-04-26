d5c23c4c04e928fec1d9a0808df289b37257e384
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-04-26 19:00:49 -0300

    Configurações de segurança do usuário

--- src/main/java/com/grupo1/ecommerce/security/BasicSecurityConfig.java
+++ src/main/java/com/grupo1/ecommerce/security/BasicSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	
