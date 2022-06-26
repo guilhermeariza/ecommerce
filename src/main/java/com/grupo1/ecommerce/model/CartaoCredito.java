c4c427b3d9b245181291d00cd9c7044b788707a2
Guilherme Ariza <gui.ariza@gmail.com>
2022-06-26 19:21:31 -0300

    JsonIgnoreProperties retirado do Cartão de Crédito

--- src/main/java/com/grupo1/ecommerce/model/CartaoCredito.java
+++ src/main/java/com/grupo1/ecommerce/model/CartaoCredito.java
@@ -10,7 +10,6 @@ import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_cartao_credito", uniqueConstraints = {@UniqueConstraint(columnNames = {"numeroCartao"})})
